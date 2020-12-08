84a0874315633ff4a7e3362a1598cc7fbb730dda
ShimiShimson <szadam11@gmail.com>
2020-12-08 07:51:10 +0100

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,31 @@
-import { Component } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { GoogleBooksService } from './book-list/books.service';
-import { selectBooks } from './state/book.selectors';
-import { selectBookCollection } from './state/book.selectors';
-import { retrievedBookList } from './state/books.actions';
-import { removeBook } from './state/books.actions';
-import { addBook } from './state/books.actions';
+import { Component } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+
+import { selectBookCollection, selectBooks } from "./state/books.selectors";
+import { retrievedBookList, addBook, removeBook } from "./state/books.actions";
+import { GoogleBooksService } from "./book-list/books.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
   books$ = this.store.pipe(select(selectBooks));
-  booksCollection$ = this.store.pipe(select(selectBookCollection));
+  bookCollection$ = this.store.pipe(select(selectBookCollection));
 
   onAdd(bookId) {
     this.store.dispatch(addBook({ bookId }));
   }
 
-   onRemove(bookId) {
+  onRemove(bookId) {
     this.store.dispatch(removeBook({ bookId }));
   }
 
-  constructor(
-    private booksService: GoogleBooksService,
-    private store: Store
-  ) {}
+  constructor(private booksService: GoogleBooksService, private store: Store) {}
 
   ngOnInit() {
     this.booksService
       .getBooks()
-      .subscribe((Book) => 
-this.store.dispatch(retrievedBookList({ Book })));
+      .subscribe((Book) => this.store.dispatch(retrievedBookList({ Book })));
   }
 }
-
